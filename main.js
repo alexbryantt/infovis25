b84e5d70e308095c64c6360edd6e4b8f7dcfd3ad
Alexandra Bryant <alexandrabryant@Alexandras-MacBook-Pro-1673.local>
2025-03-24 19:07:08 -0400

    some html + js

---
+++ main.js
@@ -0,0 +1,34 @@
+let keyframes = [
+    {
+        activeVerse: 1, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 2, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 3, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 4, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 5, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 6, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 7, 
+        activeLines: [1, 2, 3],
+    },
+    {
+        activeVerse: 8, 
+        activeLines: [1, 2, 3],
+    }
+]
